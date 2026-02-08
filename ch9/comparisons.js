2d02e1676302c6370ec333c17d941f44119fcf8a
Drajat wibowo <drajatwibowo001@gmail.com>
2026-02-08 22:12:12 +0700

    Add files via upload

---
+++ ch9/comparisons.js
@@ -0,0 +1,22 @@
+//=========== COMPARISONS ========================================================
+// We know many comparison operators from maths.
+
+// In JavaScript they are written like this:
+
+// Greater/less than: a > b, a < b.
+// Greater/less than or equals: a >= b, a <= b.
+// Equals: a == b, please note the double equality sign == means the equality test, while a single one a = b means an assignment.
+// Not equals: In maths the notation is ≠, but in JavaScript it’s written as a != b.
+
+//===============================================================================
+
+//================= BOOLEAN IS THE RESULT =========================================
+//All comparison operators return a boolean value:
+// true – means “yes”, “correct” or “the truth”.
+// false – means “no”, “wrong” or “not the truth”.
+alert( 2 > 1 );  // true (correct)
+alert( 2 == 1 ); // false (wrong)
+alert( 2 != 1 ); // true (correct)
+
+let result = 5 > 4; // assign the result of the comparison
+alert( result ); // true
